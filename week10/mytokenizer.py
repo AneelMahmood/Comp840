686cc2d413b685502a392337af629c6f658f8acc
Aneel Mahmood <aneelmahmood@aneels-mbp.aw4.unh.edu>
2019-11-14 11:38:56 -0500

    homework

---
+++ week10/mytokenizer.py
@@ -0,0 +1,9 @@
+#import Natural Lanuage Toolkit 
+from nltk.stem.porter import PorterStemmer
+
+porter = PorterStemmer()
+def tokenizer(text):
+    return text.split()
+
+def tokenizer_porter(text):
+    return [porter.stem(word) for word in text.split()]
